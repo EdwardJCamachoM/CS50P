4efb6f8447b39dd6747d2443a785ab7b10f1793e
Edward <edwardcamacho.m@gmail.com>
2026-02-24 02:53:57 -0500

    Update hello.py - improving program

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-name = input("What's your name? ").split().title()
-print(f"Hello, {name}")
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove white spaces from str and Capitalize user's name
+name = name.strip().title()
+
+# Split user's name into first name and last name
+
+first, last = name.split()
+
+# Say hello to user
+print("hello,", last)
+print(f"hello, {first}")
