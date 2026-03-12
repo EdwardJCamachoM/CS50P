0a4f31d30cf97b260fb299bbd0138326d993300e
Edward <edwardcamacho.m@gmail.com>
2026-03-12 02:03:42 -0500

    Working with APIs - Bitcoin price

---
+++ bitcoin.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+data = r.json()
+price = data["bitcoin"]["usd"]
+
+def main():
+        try:
+            user_input = float(input("How many bitcoins?: "))
+            bitcoin = float(user_input) * price
+        except ValueError:
+            sys.exit()
+        print(f"${bitcoin:,.4f}")
+
+if __name__ == "__main__": main()
