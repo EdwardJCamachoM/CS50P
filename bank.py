387e99201603aa6163e4bd50b545848e292652d0
Edward <edwardcamacho.m@gmail.com>
2026-03-13 23:20:43 -0500

    Update to bank to work with test_bank

--- bank.py
+++ bank.py
@@ -1,12 +1,14 @@
 def main():
     greeting = input("Insert your greeting: ").lower()
+    print(f"${value(greeting)}")
 
+def value(greeting):
     if greeting.startswith("hello"):
-        print("$0")
+        return(0)
     elif greeting.startswith("h"):
-        print("$20")
+        return(20)
     else:
-        print("$100")
+        return(100)
     
     
-main()
+if __name__ == "__main__": main()
